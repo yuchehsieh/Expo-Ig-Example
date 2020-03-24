2561486acc5f5a70756e2f51aaea8da6dceec629
Murphy Hsieh <hsiehdanny860605@gmail.com>
2020-03-24 10:55:05 +0800

    transform using FlatList

--- components/PostListTab.js
+++ components/PostListTab.js
@@ -1,44 +1,128 @@
 import React from "react";
-import {Button, StyleSheet, Text, View, Image, Dimensions, ScrollView} from "react-native";
+import {Button, StyleSheet, Text, View, Image, Dimensions, ScrollView, FlatList} from "react-native";
 import Ionicons from '@expo/vector-icons/Ionicons';
 import EvilIcons from '@expo/vector-icons/EvilIcons';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
 
+const BodyText_Type = {
+    NORMAL: 'normal',
+    LINK: 'link',
+};
+
+const DATA = [
+    {
+        id: '01',
+        name: 'therock',
+        likesCount: '985,684',
+        body: [
+            {
+                type: BodyText_Type.NORMAL,
+                text: ' Long week. 1am. Quiet time '
+            },
+            {
+                type: BodyText_Type.LINK,
+                text: '@teremana'
+            },
+            {
+                type: BodyText_Type.NORMAL,
+                text: '🥃 with brioche french toast smuthaad with peanut butter, jelly and maple syrup.\n' +
+                    'Stay healthy and positive, my friends.'
+            },
+        ]
+    },
+    {
+        id: '02',
+        name: 'avamax',
+        likesCount: '57,467',
+        body: [
+            {
+                type: BodyText_Type.NORMAL,
+                text: ' Long week. 1am. Quiet timeThrowback to sunny days ☀️'
+            }
+        ]
+    },
+    {
+        id: '03',
+        name: 'prisionbreak',
+        likesCount: '93,422',
+        body: [
+            {
+                type: BodyText_Type.NORMAL,
+                text: ' First, it was a prison. Now, it\'s a nation.\n' +
+                    'The '
+            },
+            {
+                type: BodyText_Type.LINK,
+                text: '#PrisonBreak'
+            },
+            {
+                type: BodyText_Type.NORMAL,
+                text: ' Event Series is available on Blu-ray, DVD\n' +
+                    'and Digital HD.'
+            }
+        ]
+    }
+];
+
+const RenderItem = ({name, likesCount, body, id}) => {
+    return (
+        <View style={{marginBottom: 30}} key={id}>
+            <View style={styles.rowContainer}>
+                <View style={styles.row}>
+                    <Image source={generatePath(name, 'avatar')} style={styles.avatar}/>
+                    <Text style={styles.accountName}>{name}</Text>
+                </View>
+                <Ionicons name="ios-more" size={24}/>
+            </View>
+            <Image source={generatePath(name, 'post')} style={styles.postImage}/>
+            <View style={styles.rowContainer}>
+                <View style={{...styles.row, marginTop: 10}}>
+                    <Ionicons name="md-heart-empty" size={24} style={{marginRight: 10}}/>
+                    <EvilIcons name="comment" size={30} style={{marginRight: 10}}/>
+                    <Image source={require('../assets/ChatIcon.png')} style={styles.chatIcon}/>
+                </View>
+                <Image source={require('../assets/TagIcon.png')} style={styles.tagIcon}/>
+            </View>
+            <Text style={styles.likesText}>{likesCount} likes</Text>
+            <Text style={styles.postBody}>
+                <Text style={styles.accountName}>{name}</Text>
+                {body.map(item => {
+                    if (item.type === BodyText_Type.NORMAL) {
+                        return item.text
+                    } else {
+                        return <Text style={styles.blueText}>{item.text}</Text>
+                    }
+                })}
+            </Text>
+        </View>
+    )
+};
+
+const generatePath = (name, type) => {
+    switch (name) {
+        case 'therock':
+            if (type === 'post') return require('../assets/Posts/therock_post.png');
+            if (type === 'avatar') return require('../assets/Posts/therock.png');
+        case 'avamax':
+            if (type === 'post') return require('../assets/Posts/avamax_post.png');
+            if (type === 'avatar') return require('../assets/Posts/avamax.png');
+        case 'prisionbreak':
+            if (type === 'post') return require('../assets/Posts/prisionbreak_post.png');
+            if (type === 'avatar') return require('../assets/Posts/prisionbreak.png');
+    }
+};
+
 function PostListTab({route, navigation}) {
     return (
         <View style={styles.container}>
-            {/*<Text>This is Post List</Text>*/}
-            {/*<Text>{route.params?.name || ''}</Text>*/}
-            {/*<Button*/}
-            {/*    title="Go To Chat List"*/}
-            {/*    onPress={() => navigation.navigate('ChatListScreen', {name: 'CCC'})}*/}
-            {/*/>*/}
-            <ScrollView style={styles.postContainer}>
-                <View style={styles.rowContainer}>
-                    <View style={styles.row}>
-                        <Image source={require('../assets/Posts/the_rock.png')} style={styles.avatar}/>
-                        <Text style={styles.accountName}>therock</Text>
-                    </View>
-                    <Ionicons name="ios-more" size={24}/>
-                </View>
-                <Image source={require('../assets/Posts/the_rock_post.png')} style={styles.postImage}/>
-                <View style={styles.rowContainer}>
-                    <View style={{...styles.row, marginTop: 10}}>
-                        <Ionicons name="md-heart-empty" size={24} style={{marginRight: 10}}/>
-                        <EvilIcons name="comment" size={30} style={{marginRight: 10}}/>
-                        <Image source={require('../assets/ChatIcon.png')} style={styles.chatIcon}/>
-                    </View>
-                    <Image source={require('../assets/TagIcon.png')} style={styles.tagIcon}/>
-                </View>
-                <Text style={styles.likesText}>985,684 likes</Text>
-                <Text style={styles.postBody}>
-                    <Text style={styles.accountName}>therock</Text> Long week. 1am. Quiet time <Text style={styles.blueText}>@teremana</Text>
-                    🥃 with brioche french toast smuthaad with peanut butter, jelly and maple syrup.
-                    Stay healthy and positive, my friends.
-                </Text>
-            </ScrollView>
+            <FlatList
+                data={DATA}
+                renderItem={({item}) => (
+                    <RenderItem body={item.body} name={item.name} likesCount={item.likesCount} id={item.id}/>)}
+                keyExtractor={({id}) => id}
+            />
         </View>
     );
 }
@@ -95,6 +179,5 @@ const styles = StyleSheet.create({
         color: '#002f5e'
     }
 });
-
 
 export default PostListTab;
